8fcd6937f798fc9c551fa0fb23223f0e61552b7e
jiangxlGit <836200494@qq.com>
2018-08-02 10:45:40 +0800

    add swagger2

--- mybatis-demo/springcloud-mybatis-demo1/src/main/java/com/example/demo/controller/DemoController.java
+++ mybatis-demo/springcloud-mybatis-demo1/src/main/java/com/example/demo/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Test;
 import com.example.demo.service.DemoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,12 +17,14 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/demo")
+@Api("demo1的DemoController相关api")
 public class DemoController {
 
     @Autowired
     private DemoService demoService;
 
 
+    @ApiOperation(value = "list", notes = "controller")
     @RequestMapping("/list")
     @ResponseBody
     public List<Test> list(){
@@ -28,6 +32,7 @@ public class DemoController {
     }
 
 
+    @ApiOperation(value = "save", notes = "controller")
     @RequestMapping("/save")
     @ResponseBody
     public int save(){
